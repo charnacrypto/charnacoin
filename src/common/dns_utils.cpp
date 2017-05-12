ca9e5d7193c17157bb2f1f49b72b68f9b8c8b046
tnga <devtnga@gmail.com>
2017-05-12 06:25:50 +0100

    #CHANACOIN little comments adjustement

--- src/common/dns_utils.cpp
+++ src/common/dns_utils.cpp
@@ -370,7 +370,7 @@ std::string address_from_txt_record(const std::string& s)
  *
  * gets the charnacoin address from the TXT record of the DNS entry associated
  * with <url>.  If this lookup fails, or the TXT record does not contain an
- * XMR address in the correct format, returns an empty string.  <dnssec_valid>
+ * NAC address in the correct format, returns an empty string.  <dnssec_valid>
  * will be set true or false according to whether or not the DNS query passes
  * DNSSEC validation.
  *
