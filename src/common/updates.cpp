56b799ad8508735fcc7a445e318f7cae64655db3
tnga <devtnga@gmail.com>
2017-05-15 07:09:34 +0100

    #CHARNACOIN adjust update's urls resolvation process

--- src/common/updates.cpp
+++ src/common/updates.cpp
@@ -43,6 +43,8 @@ namespace tools
     MDEBUG("Checking updates for " << buildtag << " " << software);
 
     // All four CharnacoinPulse domains have DNSSEC on and valid
+    // @TODO:#CHARNACOIN either to have dns pulses for this, it will be maybe better to request update's urls from particulars links.
+    // Results can be retrieve in JSON or appriate format as dns_urls to be therefore used below
     static const std::vector<std::string> dns_urls = {
         // "updates.moneropulse.org",
     };
@@ -94,7 +96,7 @@ namespace tools
 
   std::string get_update_url(const std::string &software, const std::string &subdir, const std::string &buildtag, const std::string &version, bool user)
   {
-    const char *base = user ? "" : "";
+    const char *base = user ? "https://charnacoin.com/downloads" : "https://charnacoin.com/updates";
 #ifdef _WIN32
     static const char extension[] = ".zip";
 #else
