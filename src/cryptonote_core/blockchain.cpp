634ebfc2596613905f8396669586d785c098e3c8
tnga <devtnga@gmail.com>
2017-05-24 02:41:48 +0100

    #CHARNACOIN adjust hard fork switchen timestamp
    have to not forget to adjust starting block for hard fork version higher than 1

--- src/cryptonote_core/blockchain.cpp
+++ src/cryptonote_core/blockchain.cpp
@@ -87,7 +87,7 @@ static const struct {
   time_t time;
 } mainnet_hard_forks[] = {
   // version 1 from the start of the blockchain
-  { 1, 1, 0, 1341378000 },
+  { 1, 1, 0, 1483228801 },
 
   // version 2 starts from block 1009827, which is on or around the 20th of March, 2016. Fork time finalised on 2015-09-20. No fork voting occurs for the v2 fork.
   // { 2, 1009827, 0, 1442763710 },
@@ -99,7 +99,7 @@ static const struct {
   // { 4, 1220516, 0, 1483574400 },
   
   // version 5 starts from block 1406997, which is on or around the 20th of September, 2017. Fork time finalised on 2017-03-14.
-  { 5, 1406997, 0, 1505865600 },  
+  { 5, 1406997, 0, 1526774400 },  
 };
 static const uint64_t mainnet_hard_fork_version_1_till = 1406996;
 
